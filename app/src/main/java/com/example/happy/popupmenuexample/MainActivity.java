ec0613fe987cd63e033ca6314f91a65b6e84dcf8
Biswaranjan Parida <biswaranjanparida99@gmail.com>
2017-08-20 08:40:23 +0530

    commit by me to new repo for popupmenu proj

---
+++ app/src/main/java/com/example/happy/popupmenuexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.happy.popupmenuexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showPopup(View view){
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.popupmenu,popupMenu.getMenu());
+        popupMenu.show();
+    }
+}
